62bd98c81e1be80ab5c179f6f7b5f62ff83cef59
Alison Ghedin <alisonjghedin@gmail.com>
2022-05-31 22:38:37 -0300

    update the consumer to treat the db

--- consumer.py
+++ consumer.py
@@ -1,4 +1,5 @@
-import pika
+import pika, json
+from main import Product, db
 
 params = pika.URLParameters('amqps://liltmcjs:uuslkpHY75_na5OufuekpazLaon1uR8H@rat.rmq2.cloudamqp.com/liltmcjs')
 
@@ -11,7 +12,24 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
     print('Received in main')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(id=data['id'])
+        product['title'] = data['title']
+        product['image'] = data['image']
+        db.session.commit()
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
 
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
